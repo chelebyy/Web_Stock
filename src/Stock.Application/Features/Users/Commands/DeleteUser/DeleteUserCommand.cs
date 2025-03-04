eca18f14f2fc8aed089f45d173a19ea386cf206d
chelebyy <muhammett.celebi@gmail.com>
2025-03-04 22:03:22 +0300

    Clean: Kullanılmayan ve çakışan komut sınıfları temizlendi

--- src/Stock.Application/Features/Users/Commands/DeleteUser/DeleteUserCommand.cs
+++
@@ -1,9 +0,0 @@
-using Stock.Application.Common;
-
-namespace Stock.Application.Features.Users.Commands.DeleteUser
-{
-    public class DeleteUserCommand : ICommand
-    {
-        public int Id { get; set; }
-    }
-} 
